9ee358d997c980a6d33dc074e90692ac2f1eb8ae
KhandelwalTapan7 <133569004+KhandelwalTapan7@users.noreply.github.com>
2025-06-16 22:23:55 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace BankingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPinPrompt = new System.Windows.Forms.Label();
+            this.txtPin = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnNewPin = new System.Windows.Forms.Button();
+            this.btnCheckPinStrength = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnWithdraw = new System.Windows.Forms.Button();
+            this.btnDeposit = new System.Windows.Forms.Button();
+            this.btnTransfer = new System.Windows.Forms.Button();
+            this.btnCheckBalance = new System.Windows.Forms.Button();
+            this.btnViewMiniStatement = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(30, 30);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(100, 27);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(150, 20);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(30, 60);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(100, 57);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(150, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // lblPinPrompt
+            // 
+            this.lblPinPrompt.AutoSize = true;
+            this.lblPinPrompt.Location = new System.Drawing.Point(30, 90);
+            this.lblPinPrompt.Name = "lblPinPrompt";
+            this.lblPinPrompt.Size = new System.Drawing.Size(56, 13);
+            this.lblPinPrompt.TabIndex = 4;
+            this.lblPinPrompt.Text = "Enter PIN:";
+            // 
+            // txtPin
+            // 
+            this.txtPin.Location = new System.Drawing.Point(100, 87);
+            this.txtPin.Name = "txtPin";
+            this.txtPin.PasswordChar = '*';
+            this.txtPin.Size = new System.Drawing.Size(150, 20);
+            this.txtPin.TabIndex = 5;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(100, 120);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(100, 30);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnNewPin
+            // 
+            this.btnNewPin.Location = new System.Drawing.Point(30, 30);
+            this.btnNewPin.Name = "btnNewPin";
+            this.btnNewPin.Size = new System.Drawing.Size(100, 30);
+            this.btnNewPin.TabIndex = 7;
+            this.btnNewPin.Text = "New PIN";
+            this.btnNewPin.UseVisualStyleBackColor = true;
+            this.btnNewPin.Visible = false;
+            this.btnNewPin.Click += new System.EventHandler(this.btnNewPin_Click);
+            // 
+            // btnCheckPinStrength
+            // 
+            this.btnCheckPinStrength.Location = new System.Drawing.Point(150, 30);
+            this.btnCheckPinStrength.Name = "btnCheckPinStrength";
+            this.btnCheckPinStrength.Size = new System.Drawing.Size(100, 30);
+            this.btnCheckPinStrength.TabIndex = 8;
+            this.btnCheckPinStrength.Text = "Check PIN";
+            this.btnCheckPinStrength.UseVisualStyleBackColor = true;
+            this.btnCheckPinStrength.Visible = false;
+            this.btnCheckPinStrength.Click += new System.EventHandler(this.btnCheckPinStrength_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(270, 30);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(100, 30);
+            this.btnLogout.TabIndex = 9;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Visible = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // btnWithdraw
+            // 
+            this.btnWithdraw.Location = new System.Drawing.Point(30, 70);
+            this.btnWithdraw.Name = "btnWithdraw";
+            this.btnWithdraw.Size = new System.Drawing.Size(100, 30);
+            this.btnWithdraw.TabIndex = 10;
+            this.btnWithdraw.Text = "Withdraw";
+            this.btnWithdraw.UseVisualStyleBackColor = true;
+            this.btnWithdraw.Visible = false;
+            this.btnWithdraw.Click += new System.EventHandler(this.btnWithdraw_Click);
+            // 
+            // btnDeposit
+            // 
+            this.btnDeposit.Location = new System.Drawing.Point(150, 70);
+            this.btnDeposit.Name = "btnDeposit";
+            this.btnDeposit.Size = new System.Drawing.Size(100, 30);
+            this.btnDeposit.TabIndex = 11;
+            this.btnDeposit.Text = "Deposit";
+            this.btnDeposit.UseVisualStyleBackColor = true;
+            this.btnDeposit.Visible = false;
+            this.btnDeposit.Click += new System.EventHandler(this.btnDeposit_Click);
+            // 
+            // btnTransfer
+            // 
+            this.btnTransfer.Location = new System.Drawing.Point(270, 70);
+            this.btnTransfer.Name = "btnTransfer";
+            this.btnTransfer.Size = new System.Drawing.Size(100, 30);
+            this.btnTransfer.TabIndex = 12;
+            this.btnTransfer.Text = "Transfer";
+            this.btnTransfer.UseVisualStyleBackColor = true;
+            this.btnTransfer.Visible = false;
+            this.btnTransfer.Click += new System.EventHandler(this.btnTransfer_Click);
+            // 
+            // btnCheckBalance
+            // 
+            this.btnCheckBalance.Location = new System.Drawing.Point(30, 110);
+            this.btnCheckBalance.Name = "btnCheckBalance";
+            this.btnCheckBalance.Size = new System.Drawing.Size(100, 30);
+            this.btnCheckBalance.TabIndex = 13;
+            this.btnCheckBalance.Text = "Check Balance";
+            this.btnCheckBalance.UseVisualStyleBackColor = true;
+            this.btnCheckBalance.Visible = false;
+            this.btnCheckBalance.Click += new System.EventHandler(this.btnCheckBalance_Click);
+            // 
+            // btnViewMiniStatement
+            // 
+            this.btnViewMiniStatement.Location = new System.Drawing.Point(150, 110);
+            this.btnViewMiniStatement.Name = "btnViewMiniStatement";
+            this.btnViewMiniStatement.Size = new System.Drawing.Size(100, 30);
+            this.btnViewMiniStatement.TabIndex = 14;
+            this.btnViewMiniStatement.Text = "Mini Statement";
+            this.btnViewMiniStatement.UseVisualStyleBackColor = true;
+            this.btnViewMiniStatement.Visible = false;
+            this.btnViewMiniStatement.Click += new System.EventHandler(this.btnViewMiniStatement_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 250);
+            this.Controls.Add(this.btnViewMiniStatement);
+            this.Controls.Add(this.btnCheckBalance);
+            this.Controls.Add(this.btnTransfer);
+            this.Controls.Add(this.btnDeposit);
+            this.Controls.Add(this.btnWithdraw);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnCheckPinStrength);
+            this.Controls.Add(this.btnNewPin);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtPin);
+            this.Controls.Add(this.lblPinPrompt);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblUsername);
+            this.Name = "Form1";
+            this.Text = "Banking App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPinPrompt;
+        private System.Windows.Forms.TextBox txtPin;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnNewPin;
+        private System.Windows.Forms.Button btnCheckPinStrength;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnWithdraw;
+        private System.Windows.Forms.Button btnDeposit;
+        private System.Windows.Forms.Button btnTransfer;
+        private System.Windows.Forms.Button btnCheckBalance;
+        private System.Windows.Forms.Button btnViewMiniStatement;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BankingApp
+{
+    // Class to store transaction details
+    public class Transaction
+    {
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public decimal Amount { get; set; }
+        public decimal BalanceAfter { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Date:yyyy-MM-dd HH:mm:ss}: {Type} ${Amount:F2}, Balance: ${BalanceAfter:F2}";
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private readonly string validUsername = "user1";
+        private readonly string validPassword = "Atm2025@ind";
+        private string currentPin = "101204"; // Default PIN
+        private bool isLoggedIn = false;
+        private decimal balance = 1000m; // Initial balance
+        private List<Transaction> transactions = new List<Transaction>(); // Transaction history
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeFormState();
+        }
+
+        // Initialize form to show login controls and hide others
+        private void InitializeFormState()
+        {
+            // Show login controls
+            lblUsername.Visible = true;
+            txtUsername.Visible = true;
+            lblPassword.Visible = true;
+            txtPassword.Visible = true;
+            lblPinPrompt.Visible = true;
+            txtPin.Visible = true;
+            btnSubmit.Visible = true;
+
+            // Hide post-login controls
+            btnNewPin.Visible = false;
+            btnCheckPinStrength.Visible = false;
+            btnLogout.Visible = false;
+            btnWithdraw.Visible = false;
+            btnDeposit.Visible = false;
+            btnTransfer.Visible = false;
+            btnCheckBalance.Visible = false;
+            btnViewMiniStatement.Visible = false;
+        }
+
+        // Event handler for Submit button (login)
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string enteredUsername = txtUsername.Text.Trim();
+            string enteredPassword = txtPassword.Text.Trim();
+            string enteredPin = txtPin.Text.Trim();
+
+            if (string.IsNullOrEmpty(enteredUsername) || string.IsNullOrEmpty(enteredPassword) || string.IsNullOrEmpty(enteredPin))
+            {
+                MessageBox.Show("Please enter username, password, and PIN.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (enteredUsername != validUsername || enteredPassword != validPassword)
+            {
+                MessageBox.Show("Invalid username or password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (enteredPin.Length != 6 || !int.TryParse(enteredPin, out _) || enteredPin != currentPin)
+            {
+                MessageBox.Show("PIN must be the 6-digit number 101204.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Login successful
+            isLoggedIn = true;
+            MessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Hide login controls
+            lblUsername.Visible = false;
+            txtUsername.Visible = false;
+            lblPassword.Visible = false;
+            txtPassword.Visible = false;
+            lblPinPrompt.Visible = false;
+            txtPin.Visible = false;
+            btnSubmit.Visible = false;
+
+            // Show post-login controls
+            btnNewPin.Visible = true;
+            btnCheckPinStrength.Visible = true;
+            btnLogout.Visible = true;
+            btnWithdraw.Visible = true;
+            btnDeposit.Visible = true;
+            btnTransfer.Visible = true;
+            btnCheckBalance.Visible = true;
+            btnViewMiniStatement.Visible = true;
+
+            // Clear input fields
+            txtUsername.Clear();
+            txtPassword.Clear();
+            txtPin.Clear();
+        }
+
+        // Event handler for New PIN button
+        private void btnNewPin_Click(object sender, EventArgs e)
+        {
+            if (!isLoggedIn)
+            {
+                MessageBox.Show("Please log in first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string newPin = PromptForInput("Enter new 6-digit PIN:");
+            if (newPin == null) return; // User canceled
+
+            if (newPin.Length != 6 || !int.TryParse(newPin, out _))
+            {
+                MessageBox.Show("New PIN must be a 6-digit number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string confirmPin = PromptForInput("Confirm new PIN:");
+            if (confirmPin == null) return; // User canceled
+
+            if (newPin != confirmPin)
+            {
+                MessageBox.Show("PINs do not match. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            currentPin = newPin;
+            MessageBox.Show("New PIN set successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Event handler for Check PIN Strength button
+        private void btnCheckPinStrength_Click(object sender, EventArgs e)
+        {
+            if (!isLoggedIn)
+            {
+                MessageBox.Show("Please log in first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string pinStrength = EvaluatePinStrength(currentPin);
+            MessageBox.Show($"Your PIN is {pinStrength}.", "PIN Strength", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Event handler for Withdraw button
+        private void btnWithdraw_Click(object sender, EventArgs e)
+        {
+            if (!isLoggedIn)
+            {
+                MessageBox.Show("Please log in first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string amount = PromptForInput("Enter amount to withdraw:");
+            if (amount == null) return; // User canceled
+
+            if (!decimal.TryParse(amount, out decimal withdrawAmount) || withdrawAmount <= 0)
+            {
+                MessageBox.Show("Please enter a valid amount.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (withdrawAmount > balance)
+            {
+                MessageBox.Show("Insufficient funds.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            balance -= withdrawAmount;
+            transactions.Add(new Transaction
+            {
+                Date = DateTime.Now,
+                Type = "Withdrawal",
+                Amount = withdrawAmount,
+                BalanceAfter = balance
+            });
+
+            MessageBox.Show($"Withdrawal of ${withdrawAmount:F2} processed successfully! New balance: ${balance:F2}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Event handler for Deposit button
+        private void btnDeposit_Click(object sender, EventArgs e)
+        {
+            if (!isLoggedIn)
+            {
+                MessageBox.Show("Please log in first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string amount = PromptForInput("Enter amount to deposit:");
+            if (amount == null) return; // User canceled
+
+            if (!decimal.TryParse(amount, out decimal depositAmount) || depositAmount <= 0)
+            {
+                MessageBox.Show("Please enter a valid amount.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            balance += depositAmount;
+            transactions.Add(new Transaction
+            {
+                Date = DateTime.Now,
+                Type = "Deposit",
+                Amount = depositAmount,
+                BalanceAfter = balance
+            });
+
+            MessageBox.Show($"Deposit of ${depositAmount:F2} processed successfully! New balance: ${balance:F2}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Event handler for Transfer button
+        private void btnTransfer_Click(object sender, EventArgs e)
+        {
+            if (!isLoggedIn)
+            {
+                MessageBox.Show("Please log in first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string amount = PromptForInput("Enter amount to transfer:");
+            if (amount == null) return; // User canceled
+
+            if (!decimal.TryParse(amount, out decimal transferAmount) || transferAmount <= 0)
+            {
+                MessageBox.Show("Please enter a valid amount.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (transferAmount > balance)
+            {
+                MessageBox.Show("Insufficient funds.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string account = PromptForInput("Enter recipient account number:");
+            if (account == null) return; // User canceled
+
+            balance -= transferAmount;
+            transactions.Add(new Transaction
+            {
+                Date = DateTime.Now,
+                Type = $"Transfer to {account}",
+                Amount = transferAmount,
+                BalanceAfter = balance
+            });
+
+            MessageBox.Show($"Transfer of ${transferAmount:F2} to account {account} processed successfully! New balance: ${balance:F2}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Event handler for Check Balance button
+        private void btnCheckBalance_Click(object sender, EventArgs e)
+        {
+            if (!isLoggedIn)
+            {
+                MessageBox.Show("Please log in first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            MessageBox.Show($"Current balance: ${balance:F2}", "Balance", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Event handler for View Mini Statement button
+        private void btnViewMiniStatement_Click(object sender, EventArgs e)
+        {
+            if (!isLoggedIn)
+            {
+                MessageBox.Show("Please log in first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!transactions.Any())
+            {
+                MessageBox.Show("No transactions available.", "Mini Statement", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            var lastTransactions = transactions.OrderByDescending(t => t.Date).Take(5);
+            string statement = "Mini Statement:\n\n" + string.Join("\n", lastTransactions);
+            MessageBox.Show(statement, "Mini Statement", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Event handler for Logout button
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            isLoggedIn = false;
+            InitializeFormState();
+            MessageBox.Show("Logged out successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Helper method to evaluate PIN strength
+        private string EvaluatePinStrength(string pin)
+        {
+            if (pin.Length < 4 || pin.All(c => c == pin[0]))
+            {
+                return "Weak";
+            }
+            else if (pin.Length == 6 && pin.Distinct().Count() >= 3)
+            {
+                return "Strong";
+            }
+            else
+            {
+                return "Moderate";
+            }
+        }
+
+        // Helper method to prompt user for input
+        private string PromptForInput(string prompt)
+        {
+            Form promptForm = new Form()
+            {
+                Width = 300,
+                Height = 150,
+                Text = "Input",
+                StartPosition = FormStartPosition.CenterParent
+            };
+
+            Label lblPrompt = new Label() { Left = 20, Top = 20, Text = prompt, Width = 260 };
+            TextBox txtInput = new TextBox() { Left = 20, Top = 50, Width = 240 };
+            Button btnOk = new Button() { Text = "OK", Left = 100, Top = 80, DialogResult = DialogResult.OK };
+            Button btnCancel = new Button() { Text = "Cancel", Left = 180, Top = 80, DialogResult = DialogResult.Cancel };
+
+            // Use PasswordChar for PIN input only
+            if (prompt.Contains("PIN")) txtInput.PasswordChar = '*';
+
+            promptForm.Controls.AddRange(new Control[] { lblPrompt, txtInput, btnOk, btnCancel });
+            promptForm.AcceptButton = btnOk;
+            promptForm.CancelButton = btnCancel;
+
+            DialogResult result = promptForm.ShowDialog();
+            return result == DialogResult.OK ? txtInput.Text.Trim() : null;
+        }
+    }
+}
